f410382181f551895c29771178eea7c8e64e5c60
Jose Angel Uc Salazar <angel_salazar2017@hotmail.com>
2020-02-12 22:54:55 -0600

    video-36 validacion de roles

--- app/User.php
+++ app/User.php
@@ -18,6 +18,46 @@ class User extends Authenticatable
     }
 
     /**
+     * Resive los roles de los usuarios y valida ...
+     * De no ser que tenga un rol en el sistema se devuelve un error "401"
+     */
+    public function authorizeRoles(){
+        if ($this->hasAnyRole($roles)) {
+            return true;
+        }
+        abort(401, 'This acction is unauthirized');
+    }
+
+    /**
+     * Un usuario puede tener varios roles ...
+     * Por este motivo validamos si es que los roles que nos regresa es un array o no
+     */
+    public function hasAnyRole($roles){
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if ($this->hasRole($roles)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Validamos si el usuario cunta con un rol en el sistema
+     */
+    public function hasRole($role){
+        if ($this->roles()->where('name',$role)->first()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
