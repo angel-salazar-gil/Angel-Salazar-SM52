2e8443863179de32ff4c1923ec2803b7fbf4fc29
Jose Angel Uc Salazar <angel_salazar2017@hotmail.com>
2020-02-13 19:18:10 -0600

    video-40 Listado de recursos

---
+++ resources/assets/js/components/PokemonsComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <div class="col-sm" v-for="pokemon in pokemons">
+            <div class="card text-center" style="width: 18rem; margin-top: 70px;">
+            <img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;" class="card-img-top rounded-circle mx-auto d-block" src="images/" alt="Pokemon">
+                <div class="card-body">
+                    <h5 class="card-title">{{ pokemon.name }}</h5>
+                    <p class="card-text">{{ pokemon.description }}</p>
+                    <a href="/trainers/" class="btn btn-primary">Ver más...</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                pokemons: [
+                    {id:1, name:"Pikachu", description:"Pokemon Electrico"},
+                    {id:2, name:"Squirtle", description:"Pokemon Acuatico"},
+                    {id:3, name:"Charizard", description:"Pokemon de Fuego"}
+                ]
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/pokemons/index.blade.php
+++ resources/views/pokemons/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
     @section('content')
         <h1>Hola Que Hace !!!</h1>
-        <example-component></example-component>
-    @endsection
+        <pokemons-component></pokemons-component>
+        @endsection
